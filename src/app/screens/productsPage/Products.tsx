139b461d38efe9fb6adf1a72f58e83653577a9af
Azi-png <azizagoibnazarova24@gmail.com>
2025-05-22 07:08:20 +0900

    fix: modify basket business logic via customised hooks

--- src/app/screens/productsPage/Products.tsx
+++ src/app/screens/productsPage/Products.tsx
@@ -262,7 +262,6 @@ export default function ProductsPage(props: ProductsProps) {
                         <Button
                           className={"shop-btn"}
                           onClick={(e) => {
-                            console.log("BUTTON PRESSED!");
                             onAdd({
                               _id: product._id,
                               quantity: 1,
