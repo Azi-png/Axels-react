0014cf6b2048882f13e7a35c0504f489c1268488
Azi-png <azizagoibnazarova24@gmail.com>
2025-05-23 11:08:49 +0900

    feat: create sign up process

--- src/app/sevices/MemberService.ts
+++ src/app/sevices/MemberService.ts
@@ -35,6 +35,23 @@ class MemberService {
       throw err;
     }
   }
+
+  public async signup(input: MemberInput): Promise<Member> {
+    try {
+      const url = this.path + "/member/signup";
+      const result = await axios.post(url, input, { withCredentials: true });
+      console.log("signup:", result);
+
+      const member: Member = result.data.member;
+      console.log("member:", member);
+      localStorage.setItem("memberData", JSON.stringify(member));
+
+      return member;
+    } catch (err) {
+      console.log("Error, signup:", err);
+      throw err;
+    }
+  }
 }
 
 export default MemberService;
